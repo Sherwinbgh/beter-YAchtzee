93794d6f81b51c2f9ac94e00b055c8f26c99c17b
Shaqwin <50031446+Sherwinbgh@users.noreply.github.com>
2024-03-18 09:58:25 +0100

    telldubbelsteen en begin van rekenen

--- Yachtzee.js
+++ Yachtzee.js
@@ -2,6 +2,9 @@ let player1Score=[];
 let player1Dice=[];
 let rollCount=0;
 let roundCount=0;
+let lijstdubbelsteen=[];
+var telldubbelsteen = {};
+var maaklijstvanobjects = [];
 
 const canthrow = [false,false,false,false,false];
 const rollButton = document.getElementById("roll");
@@ -9,40 +12,38 @@ const diceElements = document.querySelectorAll(".dice");
 const scoreTableCells = document.querySelectorAll(".cell");
 const dices = document.querySelectorAll(".dice");
 const allecells=document.querySelectorAll(".cell");
-//roll dice werkend maar moet nog de fotos eruit halen
+//roll dice werkend
 console.log(dices);
 function rollDice() {
     rollCount++;
     let randomDice = [];
-
     for(var i = 0; i < 5;i++){
         if(canthrow[i]){
             continue;
         }
-        dices [i].src = "dice_images/Dice-" + randomnumber() + ".png";
+        var randomnumber2 = randomnumber()
+        dices [i].src = "dice_images/Dice-" + randomnumber2 + ".png";
+        lijstdubbelsteen [i] = randomnumber2;
     }
-    console.log(randomDice);
+    sumup();
   }
+//hold function met bg color
 dices.forEach((dice,index) => {
     dice.onclick = function(){
         if(canthrow[index]){
             canthrow[index] = false;
+            dice.style.border = "none";
         }
         else{
             canthrow[index] = true;
+            dice.style.border = "3px solid red";
         }
-        console.log(canthrow)
     }
 });
-
+//random number generator
 function randomnumber(){
     let random = Math.floor(Math.random()*6)+1;
     return random;
-}
-function resetGameState() {
-    rollsLeft = 3;
-    selectedDice = [];
-    updateRollsLeft();
 }
 // forloop
 let cells=[];
@@ -55,16 +56,64 @@ function selectcells(){
         cells.push(allecells[i])
     }
 }
-
-//nummer veranderd van rood naar zwart alleen moet getest worden
-/*
-function changecolor(){
-    var cell = document.getElementById();
-    if(cell.style.color === 'red'){
-        cell.style.color === 'black';
-    } else {
-        cell.style.color ==='red';
-    }
+//dubbelstenen in de console opslaan
+function sumup(){
+    telldubbelsteen = {};
+    lijstdubbelsteen.forEach(digit => {
+        if(telldubbelsteen[digit] == undefined){
+            telldubbelsteen[digit] = 0;
+        };
+        telldubbelsteen[digit] += 1;
+    });
+    return telldubbelsteen;
 }
-*/
 //rekenen
+function calculateones(){
+    return telldubbelsteen[1];
+}
+function calculateTwos(){
+    return telldubbelsteen[2]*2;
+}
+function calculateThrees(){
+    return telldubbelsteen[3]*3;
+}
+function calculatefours(){
+    return telldubbelsteen[4]*4;
+}
+function calculatefives(){
+    return telldubbelsteen[5]*5;
+}
+function calculatesixs(){
+    return telldubbelsteen[6]*6;
+}
+function maaklijstvanobject(){
+    var maaklijstvanobjects =  Object.values(telldubbelsteen);
+    return maaklijstvanobjects;
+}
+function threeofakinds(){
+    maaklijstvanobject();
+    return maaklijstvanobjects.includes(3);
+}
+function fourofakinds(){
+    maaklijstvanobject();
+    return maaklijstvanobjects.includes(4);
+}
+function fullhouce(){
+    maaklijstvanobject();
+    return maaklijstvanobjects.includes(3,2);
+}
+function smallstraught(){
+    maaklijstvanobject();
+    return maaklijstvanobjects.includes(1,2,3,4);
+}
+function largestraight(){
+    maaklijstvanobject();
+    return maaklijstvanobjects.includes(1,2,3,4,5);
+}
+function chance(){
+
+}
+function yachtzee(){
+    maaklijstvanobject();
+    return maaklijstvanobjects.includes(5);
+}
